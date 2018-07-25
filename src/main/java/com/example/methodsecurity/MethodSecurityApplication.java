d27d2e4793055b74bb8753bc723eff2c478998dd
joshlong <josh@joshlong.com>
2018-07-24 17:28:58 -0700

    mthod security

--- src/main/java/com/example/methodsecurity/MethodSecurityApplication.java
+++ src/main/java/com/example/methodsecurity/MethodSecurityApplication.java
@@ -1,10 +1,7 @@
 package com.example.methodsecurity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,14 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,6 +25,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -43,15 +41,27 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-@SpringBootApplication
+@EnableJpaAuditing
 @EnableGlobalMethodSecurity(
+	prePostEnabled = true,
 	jsr250Enabled = true,
-	securedEnabled = true,
-	prePostEnabled = true
+	securedEnabled = true
 )
+@SpringBootApplication
 public class MethodSecurityApplication {
 
-		// this is from spring-security-data
+		@Bean
+		AuditorAware<String> auditor() {
+				return () -> {
+						SecurityContext context = SecurityContextHolder.getContext();
+						Authentication authentication = context.getAuthentication();
+						if (null != authentication) {
+								return Optional.ofNullable(authentication.getName());
+						}
+						return Optional.empty();
+				};
+		}
+
 		@Bean
 		SecurityEvaluationContextExtension securityEvaluationContextExtension() {
 				return new SecurityEvaluationContextExtension();
@@ -62,19 +72,18 @@ public class MethodSecurityApplication {
 		}
 }
 
-@Slf4j
+
+@Transactional
 @Component
+@Log4j2
 class Runner implements ApplicationRunner {
 
 		private final UserRepository userRepository;
 		private final AuthorityRepository authorityRepository;
 		private final MessageRepository messageRepository;
-		private final UserRepositoryUserDetailsService userDetailsService;
+		private final UserDetailsService userDetailsService;
 
-		Runner(UserRepository userRepository,
-									AuthorityRepository authorityRepository,
-									MessageRepository messageRepository,
-									UserRepositoryUserDetailsService userDetailsService) {
+		Runner(UserRepository userRepository, AuthorityRepository authorityRepository, MessageRepository messageRepository, UserDetailsService userDetailsService) {
 				this.userRepository = userRepository;
 				this.authorityRepository = authorityRepository;
 				this.messageRepository = messageRepository;
@@ -82,206 +91,70 @@ class Runner implements ApplicationRunner {
 		}
 
 		private void authenticate(String username) {
-				UserDetails principal = this.userDetailsService.loadUserByUsername(username);
-				Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+				UserDetails user = this.userDetailsService.loadUserByUsername(username);
+				Authentication authentication = new UsernamePasswordAuthenticationToken(user,
+					user.getPassword(), user.getAuthorities());
 				SecurityContextHolder.getContext().setAuthentication(authentication);
-				log.info("authenticated " + username + ".");
-		}
-
-		private void access(String username, Long id, Function<Long, Message> msgFunction) {
-				try {
-						this.authenticate(username);
-						log.info("result for " + username + ": " + msgFunction.apply(id));
-				}
-				catch (Exception x) {
-						log.error("oops! can't access that message as " + username + "!");
-				}
 		}
 
 		@Override
-		@Transactional
 		public void run(ApplicationArguments args) throws Exception {
+				// install some data
+				Authority user = this.authorityRepository.save(new Authority("USER")),
+					admin = this.authorityRepository.save(new Authority("ADMIN"));
 
-				// lets install some data!
-				Authority admin = this.authorityRepository.save(new Authority("ADMIN")),
-					user = this.authorityRepository.save(new Authority("USER"));
+				User josh = this.userRepository.save(new User("josh", "password", user));
+				User rob = this.userRepository.save(new User("rob", "password", admin, user));
 
-				User josh = this.userRepository.save(new User("jlong", "password", user)),
-					rob = this.userRepository.save(new User("rwinch", "password", user, admin));
+				Message messageForRob = this.messageRepository.save(new Message("hi Rob!", rob));
+				this.messageRepository.save(new Message("Hi 1", rob));
+				this.messageRepository.save(new Message("Hi 2", rob));
 
-				Message forRob = this.messageRepository.save(new Message("this is a message for Rob", rob));
+				this.messageRepository.save(new Message("Hi 1", josh));
 
-				log.info("josh: " + this.userDetailsService.loadUserByUsername(josh.getEmail()));
-				log.info("rob: " + this.userDetailsService.loadUserByUsername(rob.getEmail()));
 
-				access(josh.getEmail(), forRob.getId(), messageRepository::findByIdPreAuthorize);
-				access(rob.getEmail(), forRob.getId(), messageRepository::findByIdPreAuthorize);
+				log.info("josh: " + josh.toString());
+				log.info("rob: " + rob.toString());
 
-				access(josh.getEmail(), forRob.getId(), messageRepository::findByIdRolesAllowed);
-  		access(rob.getEmail(), forRob.getId(), messageRepository::findByIdRolesAllowed);
+				attemptAccess(rob.getEmail(), josh.getEmail(),
+					messageForRob.getId(), this.messageRepository::findByIdRolesAllowed);
 
-				access(josh.getEmail(), forRob.getId(), messageRepository::findByIdSecured);
-				access(rob.getEmail(), forRob.getId(), messageRepository::findByIdSecured);
+				attemptAccess(rob.getEmail(), josh.getEmail(),
+					messageForRob.getId(), this.messageRepository::findByIdSecured);
 
-				access(josh.getEmail(), forRob.getId(), messageRepository::findByIdBeanCheck);
-				access(rob.getEmail(), forRob.getId(), messageRepository::findByIdBeanCheck);
+				attemptAccess(rob.getEmail(), josh.getEmail(),
+					messageForRob.getId(), this.messageRepository::findByIdPreAuthorize);
+
+				attemptAccess(rob.getEmail(), josh.getEmail(), messageForRob.getId(),
+					this.messageRepository::findByIdPostAuthorize);
+
 
 				authenticate(rob.getEmail());
-				this.messageRepository.findMessagesFor(PageRequest.of(0, 5)).forEach(msg -> log.info("found " + msg));
+				this.messageRepository.findMessagesFor(PageRequest.of(0, 5)).forEach(log::info);
 
 				authenticate(josh.getEmail());
-				this.messageRepository.findMessagesFor(PageRequest.of(0, 5)).forEach(msg -> log.info("found " + msg));
+				this.messageRepository.findMessagesFor(PageRequest.of(0, 5)).forEach(log::info);
+
+
+				log.info("audited: " + this.messageRepository.save(new Message("this is a test", rob)));
 
 		}
 
+		private void attemptAccess(String adminUser,
+																													String regularUser,
+																													Long msgId,
+																													Function<Long, Message> fn) {
+				authenticate(adminUser);
+				log.info("result for Rob:" + fn.apply(msgId));
 
-}
+				try {
+						authenticate(regularUser);
+						log.info("result for Josh:" + fn.apply(msgId));
+				}
+				catch (Throwable e) {
+						log.error("oops! couldn't obtain the result for Josh");
+				}
 
-interface UserRepository extends JpaRepository<User, Long> {
-		User findByEmail(String email);
-}
-
-interface AuthorityRepository extends JpaRepository<Authority, Long> {
-		Authority findByAuthority(String a);
-}
-
-@Service("authz")
-class AuthService {
-
-		public boolean check(Message msg, User user) {
-				return msg.getTo().getId().equals(user.getId());
-		}
-}
-
-interface MessageRepository extends JpaRepository<Message, Long> {
-
-		String QUERY = "select m from Message m where m.id = ?1";
-
-		@Query(QUERY)
-		@PostAuthorize("@authz.check( returnObject , principal?.user  )")
-		Message findByIdBeanCheck(Long id);
-
-		@RolesAllowed("ROLE_ADMIN")
-		@Query(QUERY)
-		Message findByIdRolesAllowed(Long id);
-
-		@Secured("ROLE_ADMIN")
-		@Query(QUERY)
-		Message findByIdSecured(Long id);
-
-		@PreAuthorize("hasRole('ADMIN')")
-		@Query(QUERY)
-		Message findByIdPreAuthorize(Long id);
-
-		@Query("select m from Message m where m.to.id = ?#{ principal?.user?.id }")
-		Page<Message> findMessagesFor(Pageable pageable);
-}
-
-@Data
-@AllArgsConstructor
-@ToString(exclude = "users")
-@Entity
-@EqualsAndHashCode(exclude = "users")
-class Authority {
-
-		@Id
-		@GeneratedValue
-		private Long id;
-
-		private String authority;
-
-		Authority() {
-		}
-
-		Authority(String authority) {
-				this(authority, new HashSet<>());
-		}
-
-		Authority(String authority, Set<User> users) {
-				this.authority = authority;
-				this.users.addAll(users);
-		}
-
-		@ManyToMany(cascade = {
-			CascadeType.PERSIST,
-			CascadeType.MERGE
-		})
-		@JoinTable(name = "authority_user",
-			joinColumns = @JoinColumn(name = "authority_id"),
-			inverseJoinColumns = @JoinColumn(name = "user_id")
-		)
-		private List<User> users = new ArrayList<>();
-
-}
-
-
-@Data
-@AllArgsConstructor
-@EqualsAndHashCode(exclude = "authorities")
-@Entity
-class User {
-
-		@Id
-		@GeneratedValue
-		private Long id;
-		private String email;
-		private String password;
-
-		@ManyToMany(mappedBy = "users")
-		private List<Authority> authorities = new ArrayList<>();
-
-		User() {
-				this(null, null, new HashSet<>());
-		}
-
-		User(String u, String pw, Set<Authority> authorities) {
-				this.email = u;
-				this.password = pw;
-				this.authorities.addAll(authorities);
-		}
-
-		User(String u, String pw, Authority... auths) {
-				this(u, pw, new HashSet<>(Arrays.asList(auths)));
-		}
-
-		User(String e, String pw) {
-				this(e, pw, new HashSet<>());
-		}
-}
-
-@Entity
-@Data
-@AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-class Message {
-
-		@Id
-		@GeneratedValue
-		private Long id;
-
-		private String text;
-
-		@OneToOne
-		private User to;
-
-		@LastModifiedDate
-		private Date lastModifiedDate;
-
-		@CreatedDate
-		private Date createdDate;
-
-		@LastModifiedBy
-		private String lastModifiedBy;
-
-		@CreatedBy
-		private String createdBy;
-
-		Message() {
-		}
-
-		public Message(String text, User to) {
-				this.text = text;
-				this.to = to;
 		}
 }
 
@@ -289,34 +162,28 @@ class Message {
 @Service
 class UserRepositoryUserDetailsService implements UserDetailsService {
 
-		private final UserRepository users;
+		private final UserRepository userRepository;
 
-		public UserRepositoryUserDetailsService(UserRepository users) {
-				this.users = users;
+		UserRepositoryUserDetailsService(UserRepository userRepository) {
+				this.userRepository = userRepository;
 		}
 
-		@Override
-		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				User user = users.findByEmail(username);
-				if (null == user) {
-						throw new UsernameNotFoundException("can't find " + username + "!");
-				}
-				return new CustomUserDetails(user);
-		}
-
-		@ToString
-		static final class CustomUserDetails implements UserDetails {
+		public static class UserUserDetails implements UserDetails {
 
 				private final User user;
 
-				private final Collection<GrantedAuthority> authorities;
+				private final Set<GrantedAuthority> authorities;
 
-				CustomUserDetails(User user) {
+				public UserUserDetails(User user) {
 						this.user = user;
 						this.authorities = this.user.getAuthorities()
 							.stream()
 							.map(au -> new SimpleGrantedAuthority("ROLE_" + au.getAuthority()))
 							.collect(Collectors.toSet());
+				}
+
+				public User getUser() {
+						return user;
 				}
 
 				@Override
@@ -329,13 +196,9 @@ class UserRepositoryUserDetailsService implements UserDetailsService {
 						return this.user.getPassword();
 				}
 
-				public User getUser() {
-						return user;
-				}
-
 				@Override
 				public String getUsername() {
-						return user.getEmail();
+						return this.user.getEmail();
 				}
 
 				@Override
@@ -358,4 +221,150 @@ class UserRepositoryUserDetailsService implements UserDetailsService {
 						return true;
 				}
 		}
+
+		@Override
+		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				User usr = this.userRepository.findByEmail(username);
+				if (null != usr) {
+						return new UserUserDetails(usr);
+				}
+				else throw new UsernameNotFoundException("couldn't find " + username + "!");
+		}
+}
+
+interface MessageRepository extends JpaRepository<Message, Long> {
+
+		String QUERY = "select m from Message m where m.id = ?1";
+
+		@Query(QUERY)
+		@RolesAllowed("ROLE_ADMIN")
+		Message findByIdRolesAllowed(Long id);
+
+		@Query(QUERY)
+		@Secured("ROLE_ADMIN")
+		Message findByIdSecured(Long id);
+
+
+		@Query(QUERY)
+		@PreAuthorize("hasRole('ADMIN')")
+		Message findByIdPreAuthorize(Long id);
+
+		@Query(QUERY)
+		@PostAuthorize("@authz.check(returnObject, principal?.user )")
+		Message findByIdPostAuthorize(Long id);
+
+		@Query("select m from Message m where m.to.id = ?#{  principal?.user?.id  }")
+		Page<Message> findMessagesFor(Pageable pageable);
+}
+
+@Log4j2
+@Service("authz")
+class AuthService {
+
+		public boolean check(Message msg, User user) {
+				log.info("checking " + user.getEmail() + "..");
+				return msg.getTo().getId().equals(user.getId());
+		}
+}
+
+interface UserRepository extends JpaRepository<User, Long> {
+
+		User findByEmail(String email);
+}
+
+interface AuthorityRepository extends JpaRepository<Authority, Long> {
+}
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+class Message {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String text;
+
+		@OneToOne
+		private User to;
+
+		@CreatedBy
+		private String createdBy;
+
+		@CreatedDate
+		@Temporal(TemporalType.TIMESTAMP)
+		private Date created;
+
+		public Message(String text, User to) {
+				this.text = text;
+				this.to = to;
+		}
+}
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = "authorities")
+@Data
+class User {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String email, password;
+
+		@ManyToMany(mappedBy = "users")
+		private List<Authority> authorities = new ArrayList<>();
+
+		public User(String email, String password, Set<Authority> authorities) {
+				this.email = email;
+				this.password = password;
+				this.authorities.addAll(authorities);
+		}
+
+		public User(String email, String password) {
+				this(email, password, new HashSet<>());
+		}
+
+		public User(String email, String password, Authority... authorities) {
+				this(email, password, new HashSet<>(Arrays.asList(authorities)));
+		}
+}
+
+@Entity
+@AllArgsConstructor
+@ToString(exclude = "users")
+@NoArgsConstructor
+@Data
+class Authority {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String authority;
+
+		public Authority(String authority) {
+				this.authority = authority;
+		}
+
+		public Authority(String authority, Set<User> users) {
+				this.authority = authority;
+				this.users.addAll(users);
+		}
+
+		@ManyToMany(cascade = {
+			CascadeType.PERSIST, CascadeType.MERGE
+		})
+		@JoinTable(
+			name = "authority_user",
+			joinColumns = @JoinColumn(name = "authority_id"),
+			inverseJoinColumns = @JoinColumn(name = "user_id")
+		)
+		private List<User> users = new ArrayList<>();
+
 }
